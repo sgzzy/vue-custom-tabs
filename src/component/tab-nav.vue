ae768900a20ca7f2f7f24647721e20b947994247
sgzzy <witzzy@163.com>
2017-12-08 18:06:12 +0800

    change files

--- src/component/tab-nav.vue
+++ src/component/tab-nav.vue
@@ -2,7 +2,7 @@
   <div class="vue-tab-nav">
     <ul>
       <li v-for="(item, index) in labels" :key="item.name" class="curName === item.name ? 'active' : ''">
-        <a href="javascript;" @click="handleClick(item.name, $event)">{{item.label}}</a>
+        <a href="javascript;" @click="handleClick(item, item.name, $event)">{{item.label}}</a>
       </li>
     </ul>
   </div>
@@ -11,17 +11,13 @@
 export default {
   name: "vue-cunstom-tab-nav",
   props: {
-    labels: Array
+    labels: Array,
+    handleClick: Function
   },
   data: function() {
     return {
       curName: labels[0].name,
     }
   },
-  methods: {
-    handleClick: function(name, event) {
-      this.curName = name;
-    }
-  }
 }
 </script>
--- src/component/tab-panel.vue
+++ src/component/tab-panel.vue
@@ -1,8 +1,15 @@
 <template>
-
+  <div class="vue-custom-tab-content">
+    <slot> </slot>
+  </div>
 </template>
 <script>
 export default {
-  name: 'vue-custom-tab-panel'
+  name: 'vue-custom-tab-panel',
+  props: {
+    label: String,
+    disabled: Boolean,
+    name: String
+  }
 }
 </script>
